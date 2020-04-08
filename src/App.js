bcd430b38d8c9e172f1243084e30cff922014966
Rakhalbd <rakhalcu71@gmail.com>
2020-04-08 07:02:20 +0600

    thrid commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Rakhal here. React App has been created. </h1>
+        <p>This is really working!!</p>
+        <Person name="Max" age="39"></Person>
+        <Person name="Babul" age="56"></Person>
+        <Person name="Majnu" age="23">
+          My hobbies: Racing!
+        </Person>
+        <Person name="Moksed" age="31"></Person>
       </div>
     );
-  }
+  } 
 }
 
 export default App;
